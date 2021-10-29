ae5cb1ee8d940544719b2509102cf679b3ffc204
Madhasuki <madha@student.untan.ac.id>
2021-10-29 18:32:52 +0700

    laman-login real fixed

--- public/assets/js/akun.js
+++ public/assets/js/akun.js
@@ -1,10 +1,14 @@
 $(function () {
     let width = $(window).width()
 
-    if (width < 576) {
+    if (width <= 575) {
         $('.card-title').removeClass('mt-5');
         $('.login-button').removeClass('col-5');
         $('.login-button button').removeClass('float-end');
+    } else if (width < 767) {
+        $('.card-title').removeClass('mt-5');
+        $('.login-button').addClass('col-4');
+        $('.login-button').removeClass('col-5');
     } else if (width > 767 && width < 886) {
         $('.card-title').removeClass('mt-5');
         $('.login-button').removeClass('col-5');
@@ -21,10 +25,15 @@ $(function () {
     $(window).resize(function () {
         width = $(window).width()
 
-        if (width < 576) {
+        if (width <= 575) {
             $('.card-title').removeClass('mt-5');
-            $('.login-button').removeClass('col-5');
+            $('.login-button').removeClass('col-4');
+            $('.login-button').removeClass('col-3');
             $('.login-button button').removeClass('float-end');
+        } else if (width < 767) {
+            $('.card-title').removeClass('mt-5');
+            $('.login-button').addClass('col-4');
+            $('.login-button').removeClass('col-5');
         } else if (width > 767 && width < 886) {
             $('.card-title').removeClass('mt-5');
             $('.login-button').removeClass('col-5');
